1726aa4a12fe413800bba67433c2604a4ac5d4b9
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-06-24 18:35:07 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -258,7 +258,7 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
-    reply = await m.reply_text(f"**★彡 Uploading 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎🐦")
+    reply = await m.reply_text(f"**★彡 Uploading 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ ❝𝐂𝐡𝐚𝐦𝐛𝐚𝐥 𝐇𝐢𝐭𝐥𝐞𝐫𝐬 ❥❥═══ ❤️™️🐦")
     time.sleep(1)
     start_time = time.time()
     await bot.send_document(ka, caption=cc1)
