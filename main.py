829af2ffa75005cfb156830ed70d736d2da16e41
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-06-26 10:21:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ CHANNELS_LIST = [int(channel_id) for channel_id in CHANNELS if channel_id.isdigi
 
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-api_url = "http://master-api-v3.vercel.app/"
+api_url = "http://drmapijion-botupdatevip.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 token_cp ='eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'
 adda_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkcGthNTQ3MEBnbWFpbC5jb20iLCJhdWQiOiIxNzg2OTYwNSIsImlhdCI6MTc0NDk0NDQ2NCwiaXNzIjoiYWRkYTI0Ny5jb20iLCJuYW1lIjoiZHBrYSIsImVtYWlsIjoiZHBrYTU0NzBAZ21haWwuY29tIiwicGhvbmUiOiI3MzUyNDA0MTc2IiwidXNlcklkIjoiYWRkYS52MS41NzMyNmRmODVkZDkxZDRiNDkxN2FiZDExN2IwN2ZjOCIsImxvZ2luQXBpVmVyc2lvbiI6MX0.0QOuYFMkCEdVmwMVIPeETa6Kxr70zEslWOIAfC_ylhbku76nDcaBoNVvqN4HivWNwlyT0jkUKjWxZ8AbdorMLg"
@@ -726,27 +726,120 @@ async def txt_handler(bot: Client, m: Message):
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
-            elif "classplusapp.com/drm/" in url:
-                url = f"https://key-one-gamma.vercel.app/api?url={url}&token={raw_text4}"
-                #url = 'https://dragoapi.vercel.app/classplus?link=' + url
-                mpd, keys = helper.get_mps_and_keys(url)
-                url = mpd
-                keys_string = " ".join([f"--key {key}" for key in keys])
+           elif 'videos.classplusapp' in url or "tencdn.classplusapp" in url or "webvideos.classplusapp.com" in url:
+                    #codewritten by pjvip for team also help @botupdatevip 
+                    #also jion movie channel https://t.me/moviebyvip
+                    url = url.replace('https://tencdn.classplusapp.com/', 'https://media-cdn.classplusapp.com/tencent/')
+                    urlx = url 
+                    headers = {
+                    'accept': 'application/json, text/plain, */*',
+                    'accept-encoding': 'gzip',
+                    'accept-language': 'EN',
+                    'api-version': '35',
+                    'app-version': '1.4.73.2',
+                    'build-number': '35',
+                    'connection': 'Keep-Alive',
+                    'content-type': 'application/json',
+                    'device-details': 'Xiaomi_Redmi 7_SDK-32',
+                    'device-id': 'c28d3cb16bbdac01',
+                    'host': 'api.classplusapp.com',
+                    'region': 'IN',
+                    'user-agent': 'Mobile-Android',
+                    'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c',
+                    'x-access-token': f'{token_cp}',
+                    'X-CDN-Tag': 'empty'
+                }
+                     
+                    
 
-            elif "tencdn.classplusapp" in url:
-                headers = {'host': 'api.classplusapp.com', 'x-access-token': f'{raw_text4}', 'accept-language': 'EN', 'api-version': '18', 'app-version': '1.4.73.2', 'build-number': '35', 'connection': 'Keep-Alive', 'content-type': 'application/json', 'device-details': 'Xiaomi_Redmi 7_SDK-32', 'device-id': 'c28d3cb16bbdac01', 'region': 'IN', 'user-agent': 'Mobile-Android', 'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c', 'accept-encoding': 'gzip'}
-                params = {"url": f"{url}"}
-                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
-                url = response.json()['url']  
-           
-            elif 'videos.classplusapp' in url:
-                url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': f'{raw_text4}'}).json()['url']
-            
-            elif 'media-cdn.classplusapp.com' in url or 'media-cdn-alisg.classplusapp.com' in url or 'media-cdn-a.classplusapp.com' in url: 
-                headers = {'host': 'api.classplusapp.com', 'x-access-token': f'{raw_text4}', 'accept-language': 'EN', 'api-version': '18', 'app-version': '1.4.73.2', 'build-number': '35', 'connection': 'Keep-Alive', 'content-type': 'application/json', 'device-details': 'Xiaomi_Redmi 7_SDK-32', 'device-id': 'c28d3cb16bbdac01', 'region': 'IN', 'user-agent': 'Mobile-Android', 'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c', 'accept-encoding': 'gzip'}
-                params = {"url": f"{url}"}
-                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
-                url   = response.json()['url']
+                    try:
+# Send API request
+                       response = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={urlx}', headers=headers)
+                    #print(response)
+                       print(response.json())
+                       url   = response.json()['url']
+                    #urlp   = response.json()['url']
+                    #url = urlp
+                    except:
+
+                                        
+                      url = f'https://drmapijion-botupdatevip.vercel.app/api?url={urlx}'
+                      print(url)
+                      mpd = helper.get_mps_and_keys3(url) 
+                      url = mpd
+                      print(url)
+                    
+                        
+
+
+            elif 'media-cdn.classplusapp.com' in url or 'media-cdn-alisg.classplusapp.com' in url or 'media-cdn-a.classplusapp.com' in url or "cpvod" in url or 'cpvideocdn' in url:
+                    #codewritten by pjvip for team also help @botupdatevip 
+                    #also jion movie channel https://t.me/moviebyvip
+                    headers = {
+                        'accept': 'application/json, text/plain, */*',
+                        'accept-encoding': 'gzip',
+                        'accept-language': 'EN',
+                        'api-version': '35',
+                        'app-version': '1.4.73.2',
+                        'build-number': '35',
+                        'connection': 'Keep-Alive',
+                        'content-type': 'application/json',
+                        'device-details': 'Xiaomi_Redmi 7_SDK-32',
+                        'device-id': 'c28d3cb16bbdac01',
+                        'host': 'api.classplusapp.com',
+                        'region': 'IN',
+                        'user-agent': 'Mobile-Android',
+                        'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c',
+                        'x-access-token': f'{token_cp}'
+                    }
+                    urlx = url
+                    if '4b06bf8d61c41f8310af9b2624459378203740932b456b07fcf817b737fbae27' in url:
+                        pattern = re.compile(r'https://videos\.classplusapp\.com/([a-f0-9]+)/([a-zA-Z0-9]+)\.m3u8')
+                        match = pattern.match(url)
+                        if match:
+                            urlx = f"https://videos.classplusapp.com/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{match.group(2)}/{match.group(2)}.m3u8"
+                            url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={urlx}', headers=headers).json()['url']
+
+                    if "testbook.com" in url or "classplusapp.com/drm" in url or "media-cdn.classplusapp.com/drm" in url:
+                        url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
+                        url = url.replace("https://cpvideocdn.testbook.com/","https://media-cdn.classplusapp.com/drm/")
+                        urlx = url
+                        #params = (('url', f'{url}'), )
+                        try:
+                               url = f"https://drmapijion-botupdatevip.vercel.app/api?url={urlx}&token={raw_text4}" + urlx
+                               
+                               print(url)
+                               mpd, keys = helper.get_mps_and_keys2(url)
+                               url = mpd
+                               keys_string = " ".join([f"--key {key}" for key in keys])
+                               print(mpd)
+
+
+                        except:
+                               url = f"https://drmapijion-botupdatevip.vercel.app/api?url={urlx}&token={raw_text4}" + urlx
+                               
+                               print(url)
+                               mpd, keys = helper.get_mps_and_keys2(url)
+                               url = mpd
+                               keys_string = " ".join([f"--key {key}" for key in keys])
+                               print(mpd)  
+
+                    else:
+
+                        try: 
+                            resp2 = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={urlx}', headers=headers).json() 
+                            #url = resp2.json()['url']
+                            #url = resp2['url'] 
+                            if resp2['status'] ==  'ok':                          
+                                 url = resp2['url']  
+
+   
+                        except:                               
+                                url = f'https://drmapijion-botupdatevip.vercel.app/api?url={urlx}'
+                                print(url)
+                                mpd = helper.get_mps_and_keys3(url) 
+                                url = mpd
+                                print(url) 
 
             if "edge.api.brightcove.com" in url:
                 bcov = f'bcov_auth={cwtoken}'
